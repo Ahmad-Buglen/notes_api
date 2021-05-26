00ca403c9933b3d0be5a71fff610b092b637ead0
Daedalus Phyliss <dphyliss@at-d5.msk.21-school.ru>
2021-05-26 19:23:45 +0300

    add notes

---
+++ app/models/note.rb
@@ -0,0 +1,3 @@
+class Note < ApplicationRecord
+  belongs_to :admin_user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_151758) do
+ActiveRecord::Schema.define(version: 2021_05_26_160455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,4 +41,15 @@ ActiveRecord::Schema.define(version: 2021_05_26_151758) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "time"
+    t.bigint "admin_user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["admin_user_id"], name: "index_notes_on_admin_user_id"
+  end
+
+  add_foreign_key "notes", "admin_users"
 end
